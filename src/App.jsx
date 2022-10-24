4f3fccb7f8e8a5e4589d465e7311a40f07890e41
Lhorla <teteelola@gmail.com>
2022-10-24 22:54:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
 
