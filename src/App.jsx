b6f8a2003f00cd3d5a254d61bf9eb01ec2a95154
Lhorla <teteelola@gmail.com>
2022-10-28 23:01:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,20 +54,3 @@ function App() {
 export default App;
 
 
-/* 
-
-
-  function ErrorFallback({ error, resetErrorBoundary }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
-  );
-}
-
- <ErrorBoundary FallBackComponent={ErrorFallback}>
-    
-  </ErrorBoundary>
-*/
